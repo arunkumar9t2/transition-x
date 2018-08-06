b6e274f4166e1c46236dd5a45ba42e9f271d59d9
Arunkumar <arunk.beece@gmail.com>
2018-08-07 01:46:35 +0530

    Setup transition x module

--- buildSrc/src/main/java/Dependencies.kt
+++ buildSrc/src/main/java/Dependencies.kt
@@ -1,7 +1,12 @@
+object Plugins {
+    val kotlinAndroid = "kotlin-android"
+    val kotlinAndroidExt = "kotlin-android-extensions"
+}
+
 object BuildDetails {
-    val compileSdk = 27
+    val compileSdk = 28
     val minSdk = 19
-    val targetSdk = 27
+    val targetSdk = 28
     val versionCode = 1
     val versionName = "1.0"
 
@@ -11,7 +16,7 @@ object BuildDetails {
 
 
 object Versions {
-    val agp = "3.1.3"
+    val agp = "3.2.0-beta05"
     val supportLib = "27.1.1"
     val constraintLayout = "1.1.2"
     val jUnit = "4.12"
@@ -27,10 +32,14 @@ object Dependencies {
     val kotlinStdlib = "org.jetbrains.kotlin:kotlin-stdlib:${Versions.kotlin}"
     val kotlinGradlePlugin = "org.jetbrains.kotlin:kotlin-gradle-plugin:${Versions.kotlin}"
 
+    // Support
     val appCompat = "com.android.support:appcompat-v7:${Versions.supportLib}"
     val constraintLayout = "com.android.support.constraint:constraint-layout:${Versions.constraintLayout}"
     val supportDesign = "com.android.support:design:${Versions.supportLib}"
+
+    // Test
     val jUnit = "junit:junit:${Versions.jUnit}"
+    val androidJUnitRunner = "android.support.test.runner.AndroidJUnitRunner"
     val testRunner = "com.android.support.test:runner:${Versions.testRunner}"
     val espressoCore = "com.android.support.test.espresso:espresso-core:${Versions.espresso}"
 }

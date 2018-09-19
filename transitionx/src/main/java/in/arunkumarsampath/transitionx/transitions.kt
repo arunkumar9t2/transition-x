cc92f86d397e739774545b0763e936d56862519f
Arunkumar <arunk.beece@gmail.com>
2018-09-20 04:32:35 +0530

    Add documentation and bump version

--- transitionx/src/main/java/in/arunkumarsampath/transitionx/transitions.kt
+++ transitionx/src/main/java/in/arunkumarsampath/transitionx/transitions.kt
@@ -18,9 +18,28 @@ package `in`.arunkumarsampath.transitionx
 
 import `in`.arunkumarsampath.transitionx.builders.set.AutoTransitionBuilder
 import `in`.arunkumarsampath.transitionx.builders.set.DefaultTransitionSetBuilder
-import android.support.transition.TransitionManager
+import android.support.transition.*
 import android.view.ViewGroup
 
+/**
+ * Schedules a call to [TransitionManager.beginDelayedTransition] with receiver [ViewGroup] as
+ * the scene root. [transitionsBuilder] uses a [TransitionSet] instance internally and its
+ * properties can be modified by using available methods from [DefaultTransitionSetBuilder]
+ *
+ * Usage:
+ * ```
+ *  constraintLayout.prepareTransition {
+ *      changeText {
+ *          changeTextBehavior = ChangeText.CHANGE_BEHAVIOR_OUT_IN
+ *      }
+ *      +textView
+ *  }
+ *  textView.setText("Hello")
+ * ```
+ *
+ * @param cancel if true, ends all previous transitions before starting this transition.
+ * @param transitionsBuilder Builder to construct transition instance for use with [TransitionManager]
+ */
 inline fun ViewGroup.prepareTransition(
         cancel: Boolean = false,
         transitionsBuilder: DefaultTransitionSetBuilder.() -> Unit = {}
@@ -34,6 +53,26 @@ inline fun ViewGroup.prepareTransition(
     )
 }
 
+/**
+ * Schedules a call to [TransitionManager.beginDelayedTransition] with receiver [ViewGroup] as
+ * the scene root. [autoTransitionBuilder] uses a [AutoTransition] instance internally and its
+ * properties can be modified by using available methods from [AutoTransitionBuilder].
+ *
+ * Since [AutoTransition] is used, View bounds changes are animated using [ChangeBounds] and
+ * disappear/appear are animated using [Fade]
+ * @see [AutoTransition]
+ *
+ * Usage:
+ * ```
+ *  constraintLayout.prepareAutoTransition {
+ *      duration = 1000
+ *  }
+ *  // Layout modifications
+ * ```
+ *
+ * @param cancel if true, ends all previous transitions before starting this transition.
+ * @param autoTransitionBuilder Builder to construct transition instance for use with [TransitionManager]
+ */
 inline fun ViewGroup.prepareAutoTransition(
         cancel: Boolean = false,
         autoTransitionBuilder: AutoTransitionBuilder.() -> Unit = {}
@@ -45,4 +84,19 @@ inline fun ViewGroup.prepareAutoTransition(
             this,
             AutoTransitionBuilder().apply(autoTransitionBuilder).transition
     )
+}
+
+/**
+ * Creates a [Transition] instance by configuring it properties provided by [DefaultTransitionSetBuilder]
+ *
+ * Usage:
+ * ```
+ *  val moveTransition = transitionSet {
+ *      moveResize()
+ *      scaleRotate()
+ *  }
+ * ```
+ */
+inline fun transitionSet(transitionsBuilder: DefaultTransitionSetBuilder.() -> Unit = {}): Transition {
+    return DefaultTransitionSetBuilder().apply(transitionsBuilder).transition
 }
